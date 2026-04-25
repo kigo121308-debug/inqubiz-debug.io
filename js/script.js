65bab25fb8e4cd3ab7630bd6d81b10afc6e344a9
kigo121308-debug <kigo121308@gmail.com>
2026-04-25 17:45:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const FAQ_question = document.getElementById('FAQ_question');
+const FAQ_answer = document.getElementById('FAQ_answer');
+
+FAQ_question.addEventListener('click', () => {
+    FAQ_answer.classList.toggle('appear');
+});
